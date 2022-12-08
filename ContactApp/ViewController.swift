530fe73555086d2ec5a6ec84e7d59ac868bbc40c
rishaban.ss <rishaban.ss@zohocorp.com>
2022-12-08 10:38:09 +0530

    Initial Commit

---
+++ ContactApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ContactApp
+//
+//  Created by rishab-13586 on 08/12/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
